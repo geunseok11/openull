a7a5a7f48e4afb8c07c57bd16163ab4b23d6a1fe
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-08-17 16:00:48 +0900

    ADD navigator

---
+++ navigator/HomeNavigator.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../pages/Home";
+import GoodsList from "../pages/GoodsList";
+import GoodsInfo from "../pages/GoodsInfo";
+import Bucket from "../pages/Bucket";
+import Search from "../pages/Search";
+
+const Stack = createStackNavigator();
+
+const HomeNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="GoodsList" component={GoodsList} />
+      <Stack.Screen name="GoodsInfo" component={GoodsInfo} />
+      <Stack.Screen name="Bucket" component={Bucket} />
+      <Stack.Screen name="Search" component={Search} />
+    </Stack.Navigator>
+  );
+};
+
+export default HomeNavigator;
