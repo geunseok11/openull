bd97faf4ce37143e6ff6961d36d2dc486e84cbb7
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-08-17 16:00:29 +0900

    ADD pages

---
+++ pages/Bucket.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+
+const Bucket = (props) => {
+  return <View></View>;
+};
+
+export default Bucket;
---
+++ pages/Search.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+
+const Search = (props) => {
+  return <View></View>;
+};
+
+export default Search;
