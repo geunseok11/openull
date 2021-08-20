4d0ab4aeab1f1f8a211e578d242645b4069ba282
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-08-21 00:12:34 +0900

    UPDATE category

--- pages/Category.js
+++ pages/Category.js
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import styled from "styled-components";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 const Category = (props) => {
-  return <View></View>;
+  return (
+    <Container>
+      <Header props={props} />
+
+      <Contents></Contents>
+
+      <Nav props={props} />
+    </Container>
+  );
 };
 
 export default Category;
