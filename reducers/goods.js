b3e10f69cafc7731d6b625a8c23d7cfc523991fb
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-08-21 11:27:49 +0900

    FIX reducer goods

--- reducers/goods.js
+++ reducers/goods.js
@@ -26,7 +26,7 @@ export const HOME_REQUEST = "HOME_REQUEST"; // home
 export const HOME_SUCCESS = "HOME_SUCCESS";
 export const HOME_FAILURE = "HOME_FAILURE";
 
-export const LOAD_BUCKET_REQUEST = "LOAD_BUCKET_REQUEST"; //  bucket, not yet patch in quantity
+export const LOAD_BUCKET_REQUEST = "LOAD_BUCKET_REQUEST"; //  bucket
 export const ADD_BUCKET_REQUEST = "ADD_BUCKET_REQUEST";
 export const DELETE_BUCKET_REQUEST = "DELETE_BUCKET_REQUEST";
 
@@ -48,19 +48,14 @@ export const homeToLoad = () => {
   };
 };
 
-// recommendation -> goodsList
 export const loadGoodsList = (data) => {
-  console.log("In REDUCER, loadGoodsList, data : ", data);
-
   return {
     type: LOAD_GOODSLIST_REQUEST,
     data,
   };
 };
 
-// search -> searchList
 export const loadSearchList = (data) => {
-  console.log("In REDUCER, loadSearchList, data : ", data);
   return {
     type: LOAD_SEARCHLIST_REQUEST,
     data,
@@ -68,12 +63,9 @@ export const loadSearchList = (data) => {
 };
 
 export const loadGoodsInfo = (id) => {
-  // console.log('In REDUCER,  loadGoodsInfo, id : ', id);
-  // const ids = id.id;
   return {
     type: LOAD_GOODSINFO_REQUEST,
     id,
-    // data:id,
   };
 };
 export const loadToBucket = () => {
@@ -82,7 +74,6 @@ export const loadToBucket = () => {
   };
 };
 export const addToBucket = (data) => {
-  console.log("In REDUCER, addToBucket, data : ", data);
   return {
     type: ADD_BUCKET_REQUEST,
     data,
@@ -96,114 +87,98 @@ export const deleteToBucket = () => {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    // bucket
     case LOAD_BUCKET_REQUEST:
       return {
         ...state,
       };
     case ADD_BUCKET_REQUEST:
-      console.log("In REDUCER, ADD_BUCKET_REQUEST, action : ", action.data);
       return {
         ...state,
         bucket: [action.data, ...state.bucket],
       };
     case DELETE_BUCKET_REQUEST:
-      console.log("In REDUCER, DELETE_BUCKET_REQUEST, action : ", action);
       return {
         ...state,
         // count: state.count - 1,
       };
 
-    // home rendering
     case HOME_REQUEST:
-      // console.log("In REDUX, HOME_REQUEST, executes")
       return {
         ...state,
-        homeLoading: true, // home rendering
+        homeLoading: true,
         homeQnADone: false,
         homeQnAError: null,
       };
     case HOME_SUCCESS:
-      console.log("In REDUX, HOME_SUCCESS, action : ", action);
       return {
         ...state,
-        homeLoading: false, // home rendering
+        homeLoading: false,
         homeQnADone: true,
         home: action.data,
       };
     case HOME_FAILURE:
       return {
         ...state,
-        homeLoading: false, // home rendering
+        homeLoading: false,
         homeQnAError: action.error,
       };
 
-    //
-
-    // load goods list
     case LOAD_GOODSLIST_REQUEST:
-      console.log("In REDUX, LOAD_GOODSLIST_REQUEST, action : ", action);
       return {
         ...state,
-        loadGoodsListLoading: true, // goods list
+        loadGoodsListLoading: true,
         loadGoodsListDone: false,
         loadGoodsListError: null,
       };
     case LOAD_GOODSLIST_SUCCESS:
-      console.log("In REDUX, LOAD_GOODSLIST_SUCCESS, action : ", action);
       return {
         ...state,
-        loadGoodsListLoading: false, // goods list
+        loadGoodsListLoading: false,
         loadGoodsListDone: true,
         goodsList: [action.data, ...state.goodsList],
       };
     case LOAD_GOODSLIST_FAILURE:
-      console.log("In REDUX, LOAD_GOODSLIST_FAILURE, action : ", action);
       return {
         ...state,
         loadGoodsListLoading: false,
-        loadGoodsListError: action.error, // home rendering
+        loadGoodsListError: action.error,
       };
 
-    // search list
     case LOAD_SEARCHLIST_REQUEST:
-      console.log("In REDUX, LOAD_SEARCHLIST_REQUEST, action : ", action);
       return {
         ...state,
-        loadSearchListLoading: true, // search list
+        loadSearchListLoading: true,
         loadSearchListDone: false,
         loadSearchListError: null,
       };
     case LOAD_SEARCHLIST_SUCCESS:
-      console.log("In REDUX, LOAD_SEARCHLIST_SUCCESS, action : ", action);
+      // console.log("In REDUX, LOAD_SEARCHLIST_SUCCESS, action : ", action);
       return {
         ...state,
-        loadSearchListLoading: false, // goods list
+        loadSearchListLoading: false,
         loadSearchListDone: true,
         searchList: [action.data, ...state.searchList],
       };
     case LOAD_SEARCHLIST_FAILURE:
-      console.log("In REDUX, LOAD_SEARCHLIST_FAILURE, action : ", action);
+      // console.log("In REDUX, LOAD_SEARCHLIST_FAILURE, action : ", action);
       return {
         ...state,
         loadSearchListLoading: false,
         loadSearchListError: action.error,
       };
 
-    // load goodsInfo rendering
     case LOAD_GOODSINFO_REQUEST:
-      // console.log("In REDUX, LOAD_GOODSINFO_REQUEST, executes, action : ", action)
       return {
         ...state,
-        loadGoodsInfoLoading: true, // goods info
+        loadGoodsInfoLoading: true,
         loadGoodsInfoDone: false,
         loadGoodsInfoError: null,
       };
     case LOAD_GOODSINFO_SUCCESS:
-      console.log("In REDUX, LOAD_GOODSINFO_SUCCESS, action : ", action);
+      // console.log("In REDUX, LOAD_GOODSINFO_SUCCESS, action : ", action);
       return {
         ...state,
-        loadGoodsInfoLoading: false, // goods info
+        loadGoodsInfoLoading: false,
         loadGoodsInfoDone: true,
         goodsInfo: action.data,
       };
@@ -211,7 +186,7 @@ const reducer = (state = initialState, action) => {
       // console.log("In REDUX, LOAD_GOODSINFO_FAILURE, ")
       return {
         ...state,
-        homeLoading: false, // home rendering
+        homeLoading: false,
         homeQnAError: action.error,
       };
 
